03ece72c5c4fffeeb5117da99a357313e2a4b495
eortiz65 <eortiz599@gmail.com>
2022-10-24 16:24:11 -0400

    added tasks and input components to start managing state

---
+++ gaming-emporium/src/components/Input.jsx
@@ -0,0 +1,9 @@
+const Input = () => {
+  return (
+    <div>
+      <h1>This is Main</h1>
+    </div>
+  )
+}
+
+export default Input
---
+++ gaming-emporium/src/components/Tasks.jsx
@@ -0,0 +1,9 @@
+const Task = () => {
+  return (
+    <div>
+      <h1>This is Main</h1>
+    </div>
+  )
+}
+
+export default Task
