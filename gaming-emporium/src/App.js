ccbf06a5df067ac45d7370afcb5bc61e4a8474dc
eortiz65 <eortiz599@gmail.com>
2022-10-24 14:39:55 -0400

    made sure to import components before i forget

--- gaming-emporium/src/App.js
+++ gaming-emporium/src/App.js
@@ -1,5 +1,8 @@
 import './App.css'
 import Main from './components/Main'
+import Search from './components/Search'
+import SideBar from './components/SideBar'
+import Catalog from './components/Catalog'
 
 function App() {
   return (
