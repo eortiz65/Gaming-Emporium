3126aa8127e1d37bedf581fc3b1ead9e76d6aae8
eortiz65 <eortiz599@gmail.com>
2022-10-28 08:04:56 -0400

    just added the CRUD comments and boilerplates

--- server.js
+++ server.js
@@ -6,3 +6,23 @@ const app = express()
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
 })
+
+//Get Routes
+app.get('/', (req, res) => {
+  res.send('get route')
+})
+
+//Post Routes
+app.post('/', (req, res) => {
+  res.send('post route')
+})
+
+//Put Routes
+app.get('/', (req, res) => {
+  res.put('put route')
+})
+
+//Delete Routes
+app.delete('/', (req, res) => {
+  res.send('delete route')
+})
