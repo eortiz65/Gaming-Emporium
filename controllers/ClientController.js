6fb42e46accda6d412f8dbbac42cca0413cf6819
eortiz65 <eortiz599@gmail.com>
2022-10-28 08:50:06 -0400

    some basic controller syntax to be used

---
+++ controllers/ClientController.js
@@ -0,0 +1,30 @@
+const getCompany = (req, res) => {
+  res.send({
+    message: 'Getting Brand'
+  })
+}
+
+const getBoardGame = (req, res) => {
+  res.send({
+    message: 'Getting BoardGame'
+  })
+}
+
+const getCardGame = (req, res) => {
+  res.send({
+    message: 'Getting Brand'
+  })
+}
+
+const getFigure = (req, res) => {
+  res.send({
+    message: 'Getting Brand'
+  })
+}
+
+module.exports = {
+  getCompany,
+  getBoardGame,
+  getCardGame,
+  getFigure
+}
