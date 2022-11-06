5c02fc3c075d54a393145c092ec8188911a0bba6
eortiz65 <eortiz599@gmail.com>
2022-11-06 12:14:52 -0500

    Catalog component with maping of figures component done

--- client/src/components/Catalog.jsx
+++ client/src/components/Catalog.jsx
@@ -1,7 +1,24 @@
-const Catalog = () => {
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Catalog = (props) => {
   return (
-    <div>
-      <li className="catalog">Catalog</li>
+    <div className="catalog-content">
+      {props.figures?.map((figure) => (
+        <Link key={figure.id} to={`/figure/${figure._id}`}>
+          <div className="figure-card" key={figure._id}>
+            <div className="figure-image">
+              <img src={figure.url} alt={`${figure.name}`} />
+            </div>
+            <div className="figure-description">
+              <h1>Name: {figure.name}</h1>
+              <h2>Manufacturer: {figure.company}</h2>
+              <p>Details: {figure.details}</p>
+            </div>
+          </div>
+        </Link>
+      ))}
     </div>
   )
 }
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,3 +1,14 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate, Routes, Route } from "react-router-dom";
+import Catalog from "./Catalog";
+import Company from "./Company"
+import CompanyForm from "./CompanyForm";
+import FigureForm from "./FigureForm";
+
+
+
+
 const Main = () => {
   return (
     <div>
